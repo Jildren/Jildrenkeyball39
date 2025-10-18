3be4e63062d42dc60d48004cf1e837df5a802334
Jildren <maru24496@gmail.com>
2025-10-18 21:44:48 +0900

    kb18によるマウス速度設定の変更

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/Jildren39/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/Jildren39/keymap.c
@@ -98,7 +98,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case QK_KB_18:
         if (record->event.pressed) {
             prev_cpi_value = keyball_get_cpi();
-            keyball_set_cpi(2); // 一時的に使うCPIを100で割った値を指定
+            keyball_set_cpi(1); // 一時的に使うCPIを100で割った値を指定
         } else {
             keyball_set_cpi(prev_cpi_value);
         }
